b214dea7ca4a6935e916051baa662cde50f0c828
YASH JAISWAL <jais.yash01@gmail.com>
2021-03-30 05:23:03 +0000

    HashTable completed

--- hashTable.js
+++ hashTable.js
@@ -15,18 +15,30 @@ class HashTable {
     const address = this._hash(key);
     if(!this.data[address]) {
       this.data[address] = [];
-      this.data[address].push([key, value]);
     }
-    console.log(this.data)
+    this.data[address].push([key, value]);
   }
 
-  get(value) {
-    const key = this._hash(value);
-    return this.data[key];
+  get(key) {
+    const address = this._hash(key);
+    if (this.data[address].length >= 1) {
+      const findData = this.data[address];
+      const data = findData.find(data => data[0] === key);
+      if (data) return data[1];
+      return undefined;
+    }
+    return undefined;
   }
 }
 
-const myHashTable = new HashTable(50);
+const myHashTable = new HashTable(2);
 // console.log(myHashTable._hash('hello'))
-myHashTable.set('boom', 10000)
+myHashTable.set('boom', 1);
+myHashTable.set('ZOOM', 2);
+myHashTable.set('GOOM', 3);
+myHashTable.set('ROOM', 4);
+myHashTable.set('lOOM', 5);
+myHashTable.set('HELLO', 6);
+myHashTable.set('gELLO', 7);
+console.log('getting property', myHashTable.get('lOO'))
 // myHashTable.set('boom', 20)
