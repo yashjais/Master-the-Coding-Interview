7fc7027203be90fceb5baec9ca2147f5a8c024b1
YASH JAISWAL <jais.yash01@gmail.com>
2021-03-30 05:53:17 +0000

    hashTable extra

--- hashTable.js
+++ hashTable.js
@@ -29,9 +29,33 @@ class HashTable {
     }
     return undefined;
   }
+
+  keys() {
+    const dataKeys = [];
+    console.log('keys', this.data.length)
+    if (!this.data.length) return dataKeys;
+    this.data.forEach(data => {
+      console.log('data', data)
+      if (data && data.length > 1) {
+        data.forEach(d => dataKeys.push(d[0]))
+      }
+      else {
+       data && dataKeys.push(data[0][0]) 
+      }
+    });
+    return dataKeys;
+  }
+
+  values() {
+    // same as keys
+  }
+
+  entries() {
+    // same as entrid
+  }
 }
 
-const myHashTable = new HashTable(2);
+const myHashTable = new HashTable(500);
 // console.log(myHashTable._hash('hello'))
 myHashTable.set('boom', 1);
 myHashTable.set('ZOOM', 2);
@@ -40,5 +64,7 @@ myHashTable.set('ROOM', 4);
 myHashTable.set('lOOM', 5);
 myHashTable.set('HELLO', 6);
 myHashTable.set('gELLO', 7);
-console.log('getting property', myHashTable.get('lOO'))
+myHashTable.set('holi', 100)
+// console.log('getting property', myHashTable.get('lOO'))
+console.log('keys', myHashTable.keys())
 // myHashTable.set('boom', 20)
