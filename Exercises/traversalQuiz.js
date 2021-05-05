3777cf56661566cf04efa295073f94237e8f9bee
YASH JAISWAL <>
2021-05-05 05:52:49 +0000

    added DFS and BFS cheatsheet

---
+++ Exercises/traversalQuiz.js
@@ -0,0 +1,17 @@
+//If you know a solution is not far from the root of the tree:
+// BFS - it starts searching closer to the root children's first
+
+//If the tree is very deep and solutions are rare:
+// BFS (DFS will take a really long time for this)
+
+//If the tree is very wide:
+// DFS (BFS will need too much of memory - will need to keep track of nodes at current level)
+
+//If solutions are frequent but located deep in the tree:
+// DFS
+
+//Determining whether a path exists between two nodes:
+// DFS
+
+//Finding the shortest path:
+// BFS
